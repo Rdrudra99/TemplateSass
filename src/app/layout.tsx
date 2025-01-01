5bb0892fa6159565258330c3525537b18ff583a6
Rudra Narayan Boitei <rdrudra99.work@gmail.com>
2025-01-01 10:21:38 -0500

    added pwa app again because have some problem in my code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <meta name="theme-color" content="#e0c8fd" />
+        {/* add keywords here  */}
+        <meta name="keywords" content="whyme" />
+        <meta property="og:title" content="Nextjs App" />
+        <meta name="twitter:title" content="Nextjs App" />
+
+        <meta name="description" content="Nextjs 14 starter template" />
+        <meta property="og:description" content="Nextjs 14 starter template" />
+        <meta name="twitter:description" content="Nextjs 14 starter template" />
+
+        <meta property="og:image" content="https://i.imgur.com/Z3bMJXy.jpg" />
+        <meta name="twitter:image" content="https://i.imgur.com/Z3bMJXy.jpg" />
+      </head>
       <body
         className={classNames("antialiased", {
           "debug-screens": process.env.NODE_ENV === "development",
